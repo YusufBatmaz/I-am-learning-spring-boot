1f0f0c225a3d9e9c20ce737f2dab1a600e6d4c8b
Yusuf Batmaz <batmazh358@gmail.com>
2025-04-19 15:43:54 +0300

    JWT + Security / Part4
    Coding the AppConfig class

---
+++ SpringBoot/jwt/src/main/java/com/yusufbatmaz/repository/UserRepository.java
@@ -0,0 +1,14 @@
+package com.yusufbatmaz.repository;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.yusufbatmaz.model.User;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+
+	Optional<User> findByUsername(String username);
+}
